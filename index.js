72f8f231650a8af1d95001051a0de836874ab98d
Neha C J <cjneha890@gmail.com>
2025-04-16 21:50:47 +0530

    getting ready to deploy

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import bodyParser from "body-parser";
 import methodOverride from "method-override";
 
 const app =express();
-const port =3000;
+const port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 let blogs=[];
 
